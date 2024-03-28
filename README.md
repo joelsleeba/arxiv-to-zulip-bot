0bd90d55fa597b341151d61f61b5933b731761a7
joelsleeba <joelsleeba1@gmail.com>
2024-03-28 14:04:04 +0530

    updates

--- README.md
+++ README.md
@@ -4,9 +4,8 @@
 1. Fork this repository
 2. Add secrets to your repository settings
   - For this go to your repository settings >> Secrets and Variables >> Actions >> New repository secret
-  - Add `ZULIP_API_URL` as the `https://yourdomainname.zulipchat.com/api/v1` (replace `yourdomainname` with your domain name)
-  - Add `ZULIP_EMAIL` as your registered email in the domain
-  - Add `ZULIP_API_KEY`. This you can get from the user settings of the Zulip.
-  - Add `ZULIP_STREAM_NAME` as the stream where you want the bot to send messages
+  - Add `ZULIP_SITE` as the `https://yourdomainname.zulipchat.com` (replace `yourdomainname` with your domain name)
+  - Add `ZULIP_EMAIL` as the email id for the bot
+  - Add `ZULIP_API_KEY`. This you can get from the bot settings in zulip
 3. Change `ARXIV_CATEGORIES` to your required categories in `main.py`
 4. Set required interval to run the script using the cron expression in `.github/workflow/zulip-bot-workflow.yml`. [This website might help](https://crontab.cronhub.io/)
--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import os
 import requests
 import time
+import zulip
 
 # Define your Zulip credentials
-ZULIP_API_URL = os.environ.get('ZULIP_API_URL')
 ZULIP_EMAIL = os.environ.get('ZULIP_EMAIL')
-ZULIP_API_KEY = os.environ.get('ZULIP_API_KEY')
-ZULIP_STREAM_NAME = os.environ.get('ZULIP_STREAM_NAME')
+ZULIP_STREAM_NAME = 'Research'
 
 # Define arXiv categories
-ARXIV_CATEGORIES = ["math.OA", "math.FA"]
+ARXIV_CATEGORIES = ["math.OA", "quant-ph", "math.FA"]
 
 # Function to send a message to Zulip
 def send_zulip_message(content):
+    client = zulip.Client(email=ZULIP_EMAIL, client='test-github-client/0.1')
     data = {
         "type": "stream",
         "to": ZULIP_STREAM_NAME,
-        "subject": "New arXiv Article",
+        "topic": "New arXiv articles",
         "content": content }
-    response = requests.post(
-        f"{ZULIP_API_URL}/messages",
-        auth=(ZULIP_EMAIL, ZULIP_API_KEY),
-        json=data
-    )
-    print(response.text)
+    client.send_message(data)
 
 # Function to fetch latest articles from arXiv
 def fetch_latest_articles(category):
@@ -33,11 +28,11 @@ def fetch_latest_articles(category):
         articles = response.text.split("<item>")
         # Skip the first element which is not an article
         for article in articles[1:]:
-            title = article.split("<title>")[1].split("</title>")[0]
-            link = article.split("<link>")[1].split("</link>")[0]
+            title = article.split("<title>")[1].split("</title>")[0].replace('$^{\\ast}$', '* ').replace('$^*$', '* ').replace('$^*$', '* ').replace("$", "$$")
             creator = article.split("<dc:creator>")[1].split("</dc:creator>")[0]
-            summary = article.split("Abstract: ")[1].split("</description>")[0].replace("$", "$$")
-            message = f"\n**{title}**\n*{creator}*\n\n{summary}\n{category}:{link}"
+            summary = article.split("Abstract: ")[1].split("</description>")[0].replace('$^\{ast}$', '* ').replace('$^*$', '* ').replace("$", "$$")
+            link = article.split("<link>")[1].split("</link>")[0]
+            message = f"\n**{title}**\n*{creator}*\n\n{summary}\n{category} : {link}"
             send_zulip_message(message)
 
 # Main function to check for new articles periodically
