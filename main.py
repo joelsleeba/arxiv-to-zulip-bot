fcf6e106c4e139dd315e05c1d752200b010817b5
joelsleeba <joelsleeba1@gmail.com>
2024-03-31 14:55:49 +0530

    code obfuscated using feedparser

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-import requests
-import time
 import zulip
 import re
-from datetime import datetime, timedelta, timezone
+import feedparser
+from datetime import datetime
 
 # Define your Zulip credentials
 ZULIP_EMAIL = os.environ.get('ZULIP_EMAIL')
@@ -14,18 +13,23 @@ ZULIP_TOPIC_NAME = 'New arXiv articles'
 ARXIV_CATEGORIES = ["math.OA", "math.FA"]
 
 # Function to send a message to Zulip
+
+
 def send_zulip_message(content):
     client = zulip.Client(email=ZULIP_EMAIL, client='test-github-client/0.1')
     data = {
         "type": "stream",
         "to": ZULIP_STREAM_NAME,
         "topic": ZULIP_TOPIC_NAME,
-        "content": content }
+        "content": content}
     client.send_message(data)
 
 # Get the url of the last article update sent to the stream
+
+
 def last_article_update_link():
-    client = zulip.Client(email=ZULIP_EMAIL, client='arxiv-bot-github-actions/0.1')
+    client = zulip.Client(
+        email=ZULIP_EMAIL, client='arxiv-bot-github-actions/0.1')
     request: Dict[str, Any] = {
         "anchor": "newest",
         "num_before": 1,
@@ -43,44 +47,57 @@ def last_article_update_link():
         if messages:
             latest_message = messages[0]
             latest_message_content = latest_message["content"]
-            url_pattern = r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
-            latest_arxiv_link = re.findall(url_pattern, latest_message_content)[0]
+            url_pattern = r'\[.*?\]\((.*?)\)'
+            latest_arxiv_link = re.findall(
+                url_pattern, latest_message_content)[0]
             return latest_arxiv_link
         else:
             return None
     else:
-        print(f"Failed to retrieve message or No previous messages")
+        print("Failed to retrieve message or No previous messages")
         return None
 
 # Function to fetch latest articles from arXiv
+
+
 def update_zulip_stream(category_list):
     url = f"http://export.arxiv.org/api/query?search_query=cat:{category_list}&sortBy=lastUpdatedDate&sortOrder=descending&start=0&max_result=100"
-    response = requests.get(url)
+    d = feedparser.parse(url)
     last_updated_article_link = last_article_update_link()
-    if response.status_code == 200:
-        articles = response.text.split("<entry>")
-        flag = True if last_updated_article_link else False # To avoid the case when not having an article ends up not getting to the execution part of the for loop
+    print(f"last updated article was : {last_updated_article_link}")
+    if d.status == 200:
+        articles = d.entries
+        # To avoid the case when not having an article ends up not getting to the execution part of the for loop
+        flag = True if last_updated_article_link else False
         # Skip the first element which is not an article
-        for article in articles[1:][::-1]: #This is to get the list in ascending order by time
-            link = article.split('<link href="')[1].split('"')[0]
+        # This is to get the list in ascending order by time
+        for article in articles[::-1]:
+            link = article.link
             if flag:
                 if link == last_updated_article_link:
                     flag = False
                 continue
-            title = article.split("<title>")[1].split("</title>")[0].replace('$^{\\ast}$', '* ').replace('$^*$', '* ').replace('$^*$', '* ').replace("$", "$$").replace("\n", " ")
-            author_list = article.split("<author>")[1:] # list of authors
-            authors = ", ".join([name.split("<name>")[1].split("</name>")[0] for name in author_list])
-            summary = article.split("<summary>")[1].split("</summary>")[0].replace('$^{\\ast}$', '* ').replace('$^*$', '* ').replace("$", "$$").replace("\n  ", "😉").replace("\n", " ").replace("😉", "\n  ")
-            category= article.split('<category term="')[1].split('"')[0]
-            message = f"\n**[{title}]({link})**\n*{authors}*\n\n{summary}\n{category}: {link}"
-            send_zulip_message(message)
+            title = article.title.replace('$^{\\ast}$', '* ').replace(
+                '$^*$', '* ').replace('$^*$', '* ').replace("$", "$$").replace("\n", " ")
+            author_list = [i.name for i in article.authors]  # list of authors
+            authors = ", ".join(author_list)
+            summary = article.summary.replace('$^{\\ast}$', '* ').replace(
+                '$^*$', '* ').replace("$", "$$").replace("\n  ", "😉").replace("\n", " ").replace("😉", "\n  ")
+            categories = ", ".join([i.term for i in article.tags])
+            message = f"\n**[{title}]({link})**\n*{authors}*\n\n{summary}\n*{categories}*"
+            print(message)
+            # send_zulip_message(message)
     else:
-        print(response.text)
+        print()
 
 # Main function to check for new articles periodically
+
+
 def main():
-    category = '+OR+cat:'.join(ARXIV_CATEGORIES) # Check https://info.arxiv.org/help/rss.html#subscribe-by-multiple-categories
+    # Check https://info.arxiv.org/help/rss.html#subscribe-by-multiple-categories
+    category = '+OR+cat:'.join(ARXIV_CATEGORIES)
     update_zulip_stream(category)
+
 
 if __name__ == "__main__":
     main()
